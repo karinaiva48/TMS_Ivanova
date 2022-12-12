d6c4f27a29e687faf6156e81639d1d1deaa9ba89
Karina Ivanova <karinaiva48@gmail.com>
2022-12-12 18:17:25 +0100

    Second task done

--- HW_11/task_2.py
+++ HW_11/task_2.py
@@ -1,18 +1,44 @@
+"""
+2. Реализуйте класс который представляет собой универсальный интерфейс по представлению температуры в шкалах 
+Цельсия/Кельвина/Фаренгейта и поддерживает конвертацию значений температуры между этими шкалами 
+(https://www.cuemath.com/temperature-conversion-formulas/)
+"""
+
 class TemperatureInterface:
 
     def __init__(self, temp) -> None:
         self.C = temp
-        self.K = temp + 273
-        self.F = ...
+        self.K = temp
+        self.F = temp
 
     @staticmethod
     def celcius_to_kelvin(temp: float):
-        return temp + 273.15
+        return round(temp + 273.15, 2)
+
+    @staticmethod
+    def celcius_to_fahrenheit(temp: float):
+        return round(temp * (9/5) + 32, 2)
     
     @staticmethod
     def kelvin_to_celcius(temp: float):
-        return temp - 273.15
-current_temp = input()
-current_temp = float(current_temp)
-x = TemperatureInterface.kelvin_to_celcius(current_temp)
-print(x)
+        return round(temp - 273.15, 2)
+
+    @staticmethod
+    def kelvin_to_fahrenheit(temp: float):
+        return round((temp - 273.15) * (9/5) + 32, 2)
+
+    @staticmethod
+    def fahrenheit_to_celsium(temp: float):
+        return round((temp - 32) * (5/9), 2)
+
+    @staticmethod
+    def fahrenheit_to_kelvin(temp: float):
+        return round((temp - 32) * (5/9) + 273.15, 2)
+
+
+print(TemperatureInterface.celcius_to_kelvin(25))
+print(TemperatureInterface.celcius_to_fahrenheit(25))
+print(TemperatureInterface.kelvin_to_celcius(285))
+print(TemperatureInterface.kelvin_to_fahrenheit(285))
+print(TemperatureInterface.fahrenheit_to_celsium(45))
+print(TemperatureInterface.fahrenheit_to_kelvin(45))
