adac35d8a18922e7ec0ef8195c1537add3ff2168
Karina Ivanova <karinaiva48@gmail.com>
2023-02-09 19:34:22 +0100

    изменения в models.py

--- HW_20_django/django_app/blog/models.py
+++ HW_20_django/django_app/blog/models.py
@@ -8,12 +8,22 @@ class User(models.Model):
     login = models.TextField()
     email = models.EmailField()
 
+    def __str__(self) -> str:
+        return f'First name: {self.first_name}, Second name: {self.second_name}'
+
+
 class Category(models.Model):
     name = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Post(models.Model):
     tittle = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-   
+    def __str__(self) -> str:
+        return f'Post: {self.tittle}, category: {self.category}'
+        
