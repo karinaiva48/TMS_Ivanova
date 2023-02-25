e19edf6f6b8ee57445fc28092d86f02a1103c3d9
Karina Ivanova <karinaiva48@gmail.com>
2023-02-25 14:58:02 +0100

    ДЗ 21

--- HW_21/mysite/myapp/urls.py
+++ HW_21/mysite/myapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('quotes/', views.KanyeWestQuote.as_view(), name='quotes'),
     path('quotes/number/<int:number>', views.KanyeWestQuote.as_view(), name='quotes'),
+    path('index/', views.index, name='index'),
 ]
 
 #path myapp/quotes/number/2
--- HW_21/mysite/myapp/views.py
+++ HW_21/mysite/myapp/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
 import requests
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 import json
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
+
+
 
 class KanyeWestQuote(View):
 
     def get(self, request, *args, **kwargs):
         number = kwargs['number'] if kwargs.get('number') else 1
-        quotes_list = [requests.get('https://api.kanye.rest').json()['quote'] for _ in range(int(number))]
+        quotes_list = [
+            requests.get('https://api.kanye.rest').json()['quote'] for _ in range(int(number))
+            ]
         return render(request, 'myapp/quotes.html', {'quotes': quotes_list})
 
+    
+def factorial_func(num):
+    if num == 0:
+        return 1
+    else:
+        return(num*factorial_func(num-1))
+
+@csrf_exempt
+@require_http_methods(['GET', 'POST'])
 def index(request: HttpRequest):
-    json.loads(request.body)
+    if request.method == 'GET':
+        return HttpResponse('Введите число:')
+    elif request.method == 'POST':
+        number = json.loads(request.body)
+        try:
+            factorial = factorial_func(number.get('number'))
+        except TypeError:
+            raise Http404('Incorrect type of message')
+        else:
+            return render(request, 'myapp/index.html', {'number': number.get('number'), 'factorial': factorial})
