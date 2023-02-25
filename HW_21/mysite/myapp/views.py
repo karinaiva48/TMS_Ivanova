285066cdf50d35987e058587a533e8b5bb2583d5
Karina Ivanova <karinaiva48@gmail.com>
2023-02-25 14:56:15 +0100

    ДЗ 21

--- HW_21/mysite/myapp/views.py
+++ HW_21/mysite/myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
 import requests
-
+from django.http import HttpRequest, HttpResponse
+import json
 
 class KanyeWestQuote(View):
 
@@ -10,3 +11,5 @@ class KanyeWestQuote(View):
         quotes_list = [requests.get('https://api.kanye.rest').json()['quote'] for _ in range(int(number))]
         return render(request, 'myapp/quotes.html', {'quotes': quotes_list})
 
+def index(request: HttpRequest):
+    json.loads(request.body)
